ddfaea299abf98fbbb191dd977e90207c1b06973
eldji225 <eldji225@gmail.com>
2025-10-30 14:56:33 +0000

    Update app.py

--- app.py
+++ app.py
@@ -90,4 +90,5 @@ def interpret():
         return jsonify({"error": "Erreur IA : impossible de générer l’interprétation. Veuillez réessayer."}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
